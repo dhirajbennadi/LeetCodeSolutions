11d813b76484e6ca4190195629b17c09a195243e
dhirajbennadi <89832913+dhirajbennadi@users.noreply.github.com>
2022-06-26 16:19:49 -0600

    Leetcode 1423

--- 1423_MaximumPointsYouCanObtainFromCards.py
+++ 1423_MaximumPointsYouCanObtainFromCards.py
@@ -1,16 +1,22 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         
+        #Sliding Window Technique using two pointers
         left = 0
         right = len(cardPoints) - k
         
+        #Calculate the sum for the first window size
         sumVal = sum(cardPoints[right:])
         
+        #Keep a copy of the first sum to compare if this value is the maximum value
         total = sumVal
         
+        #Slide the window until the right pointer is OOB
         while right < len(cardPoints):
+            #Sum of elements outside the window
             sumVal += (cardPoints[left] - cardPoints[right])
             total = max(sumVal, total)
+            #Move the window to the next position
             left += 1
             right += 1
             
